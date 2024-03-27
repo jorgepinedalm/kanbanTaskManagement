fa5f8a624c2e55861624bad3ec5bff176026cacf
jorge.pineda@insoftar.com-frontend <89551578+jorgepinedainsoftar@users.noreply.github.com>
2024-03-27 16:33:26 -0500

    feat(libs): add board service and add attribute in board model

--- libs/shared/store/src/lib/models/board.model.ts
+++ libs/shared/store/src/lib/models/board.model.ts
@@ -1,6 +1,7 @@
 import { ColumnStatus } from "./column-status.model";
 
 export interface Board{
+    idBoard:number;
     Board:number;
     name:string;
     columnStatus:ColumnStatus[]
---
+++ libs/shared/store/src/lib/services/board.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { Board } from '../models/board.model';
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BoardService {
+  private boards:Board[];
+  
+  constructor() { 
+    this.boards = [];
+  }
+
+  fetchBoards():Observable<Board[]>{
+    return of(this.boards);
+  }
+
+  getBoardById(id:number):Observable<Board | undefined>{
+    return of(this.boards.find(board => board.idBoard == id))
+  }
+
+  addBoards(boardData:Board):Observable<Board>{
+    this.boards = [...this.boards, boardData];
+    return of(this.boards[this.boards.length - 1]);
+  }
+
+  deleteBoard(id:number){
+    return of(this.boards.filter(board => board.idBoard != id));
+  }
+
+  updateBoard(payload:Board,id:number){
+    let foundBoard = this.boards.find(board => board.idBoard == id);
+    if(foundBoard){
+      foundBoard = {...foundBoard, ...payload};
+    }
+    return of(foundBoard);
+  }
+}
