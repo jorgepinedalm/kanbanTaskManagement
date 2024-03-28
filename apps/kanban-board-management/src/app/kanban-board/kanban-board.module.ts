0773fc87080f5a8e542cbc030403a4271717b5df
jorge.pineda@insoftar.com-frontend <89551578+jorgepinedainsoftar@users.noreply.github.com>
2024-03-28 13:44:38 -0500

    feat(task): add angular/cdk drag and drop
    Install package to move tasks between columns and order inside its columns.
    Changes styles and configure drag and drop props in taks and columns components

--- apps/kanban-board-management/src/app/kanban-board/kanban-board.module.ts
+++ apps/kanban-board-management/src/app/kanban-board/kanban-board.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { KanbanBoardRoutingModule } from './kanban-board-routing.module';
 import { KanbanBoardComponent } from './kanban-board.component';
 import { ColumnComponent } from '@board-management/ui';
+import {DragDropModule} from '@angular/cdk/drag-drop';
 
 @NgModule({
   declarations: [KanbanBoardComponent],
-  imports: [CommonModule, KanbanBoardRoutingModule, ColumnComponent],
+  imports: [CommonModule, KanbanBoardRoutingModule, ColumnComponent, DragDropModule],
 })
 export class KanbanBoardModule {}
--- libs/shared/ui/src/lib/column/column.component.ts
+++ libs/shared/ui/src/lib/column/column.component.ts
@@ -1,15 +1,34 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ColumnStatus } from '@board-management/shared-store';
+import { ColumnStatus, Task } from '@board-management/shared-store';
 import { TaskComponent } from '../task/task.component';
+import {DragDropModule, CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'lib-column',
   standalone: true,
-  imports: [CommonModule, TaskComponent],
+  imports: [CommonModule, TaskComponent, DragDropModule],
   templateUrl: './column.component.html',
   styleUrl: './column.component.scss',
 })
 export class ColumnComponent {
+  @Input() indexColumn:number;
   @Input() columnStatus?:ColumnStatus;
+
+  constructor(){
+    this.indexColumn = 0;
+  }
+
+  drop(event: CdkDragDrop<Task[]>) {
+    console.log({event});
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      
+      transferArrayItem(event.previousContainer.data,
+                        event.container.data,
+                        event.previousIndex,
+                        event.currentIndex);
+    }
+  }
 }
