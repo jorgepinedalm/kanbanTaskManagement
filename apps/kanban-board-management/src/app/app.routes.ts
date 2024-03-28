abc547ce759483301d3ef715b9ad56eee0ccf846
jorge.pineda@insoftar.com-frontend <89551578+jorgepinedainsoftar@users.noreply.github.com>
2024-03-28 07:21:56 -0500

    feat(board): add kanban component and configure routing from sidebar

--- apps/kanban-board-management/src/app/app.routes.ts
+++ apps/kanban-board-management/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    {
+        path: 'kanban-board/:id',
+        loadChildren: () => import('./kanban-board/kanban-board.module').then(m => m.KanbanBoardModule)
+    }
+];
