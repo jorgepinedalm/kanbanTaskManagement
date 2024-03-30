e40dbfc250136e5cd6e8fb93254db5a727c7743d
jorge.pineda@insoftar.com-frontend <89551578+jorgepinedainsoftar@users.noreply.github.com>
2024-03-30 11:48:32 -0500

    test(libs): unit test of switch component

--- libs/shared/ui/src/lib/atoms/switch/switch.component.spec.ts
+++ libs/shared/ui/src/lib/atoms/switch/switch.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { SwitchComponent } from './switch.component';
+import { InputSwitchModule } from 'primeng/inputswitch';
 
 describe('SwitchComponent', () => {
   let component: SwitchComponent;
@@ -7,7 +8,7 @@ describe('SwitchComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SwitchComponent],
+      imports: [SwitchComponent, InputSwitchModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(SwitchComponent);
@@ -17,5 +18,39 @@ describe('SwitchComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call writeValue to set the initial value', () => {
+    const initialValue = true;
+    component.writeValue(initialValue);
+  
+    expect(component.value).toEqual(initialValue);
+  });
+
+  it('should call registerOnChange to set the onChange callback', () => {
+    const onChangeCallback = jest.fn();
+    component.registerOnChange(<never>onChangeCallback);
+  
+    component.value = true;
+    expect(onChangeCallback).toHaveBeenCalledWith(true);
+  });
+
+  it('should set disabled state correctly', () => {
+    expect(component.disabled).toBe(false);
+    component.setDisabledState(true);
+    expect(component.disabled).toBe(true);
+  });
+
+  it('should set default values for readonly and disabled', () => {
+    expect(component.readonly).toBe(false);
+    expect(component.disabled).toBe(false);
+  });
+
+  it('should set inputId if provided', () => {
+    const inputId = 'test-id';
+    component.inputId = inputId;
+    fixture.detectChanges();
+    const inputElement: HTMLInputElement = fixture.nativeElement.querySelector('input');
+    expect(inputElement.getAttribute('id')).toBe(inputId);
   });
 });
