42135d81a2f5d16b604981c3ca303c63b6b14b15
jorge.pineda@insoftar.com-frontend <89551578+jorgepinedainsoftar@users.noreply.github.com>
2024-03-29 22:42:00 -0500

    test(task): create unit test of ui service

--- libs/shared/store/src/lib/services/ui.service.spec.ts
+++ libs/shared/store/src/lib/services/ui.service.spec.ts
@@ -8,9 +8,49 @@ describe('UiService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(UiService);
+    const localStorageMock = {
+      setItem: jest.fn(),
+      getItem: jest.fn()
+    };
+    Object.defineProperty(window, 'localStorage', { value: localStorageMock });
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  describe("toggleDarkMode", () => {
+    it("should set isDarkMode as true", () => {
+      service.isDarkMode = false;
+      service.toggleDarkMode(true).subscribe(status => {
+        expect(status).toBeTruthy();
+      })
+    })
+  })
+  describe("getDarkModeStatus", () => {
+    it("should set isDarkMode as true if it's save in localstorage as true", () => {
+      jest.spyOn(localStorage, "getItem").mockReturnValue("true");
+      service.isDarkMode = false;
+      service.getDarkModeStatus().subscribe(isDarkMode => {
+        expect(isDarkMode).toBeTruthy();
+      })
+    })
+  })
+  describe("toggleHideSidebar", () => {
+    it("should set hideSidebar as true", () => {
+      service.hideSidebar  = false;
+      service.toggleHideSidebar(true).subscribe(status => {
+        expect(status).toBeTruthy();
+      })
+    })
+  })
+  describe("getSideBarVisibility", () => {
+    it("should set hideSidebar as true if it's save in localstorage as true", () => {
+      jest.spyOn(localStorage, "getItem").mockReturnValue("true");
+      service.hideSidebar = false;
+      service.getSideBarVisibility().subscribe(hideSidebar => {
+        expect(hideSidebar).toBeTruthy();
+      })
+    })
+  })
 });
