e8a6d1acc6399b5ace42e9effe771c782e46975a
jorge.pineda@insoftar.com-frontend <89551578+jorgepinedainsoftar@users.noreply.github.com>
2024-03-30 11:42:08 -0500

    test(libs): unit test of button component

--- libs/shared/ui/src/lib/atoms/button/button.component.spec.ts
+++ libs/shared/ui/src/lib/atoms/button/button.component.spec.ts
@@ -18,4 +18,48 @@ describe('ButtonComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it('should emit click event when button is clicked', () => {
+    const emitSpy = jest.spyOn(component.clicked, 'emit');
+    const button = fixture.nativeElement.querySelector('button');
+    button.click();
+    expect(emitSpy).toHaveBeenCalledWith(true);
+  });
+
+  it('should set button type to "button" by default', () => {
+    const button = fixture.nativeElement.querySelector('button');
+    expect(button.getAttribute('type')).toBe('button');
+  });
+
+  it('should set button type to the specified value', () => {
+    component.type = 'submit';
+    fixture.detectChanges();
+    const button = fixture.nativeElement.querySelector('button');
+    expect(button.getAttribute('type')).toBe('submit');
+  });
+
+  it('should set inputId attribute if provided', () => {
+    component.inputId = 'testId';
+    fixture.detectChanges();
+    const button = fixture.nativeElement.querySelector('button');
+    expect(button.getAttribute('id')).toBe('testId');
+  });
+
+  it('should add specified style class to button', () => {
+    component.styleClass = 'custom-button';
+    fixture.detectChanges();
+    const button = fixture.nativeElement.querySelector('button');
+    expect(button.classList.contains('custom-button')).toBe(true);
+  });
+
+  it('should enable button by default', () => {
+    const button = fixture.nativeElement.querySelector('button');
+    expect(button.disabled).toBe(false);
+  });
+
+  it('should disable button when disabled is true', () => {
+    component.disabled = true;
+    fixture.detectChanges();
+    const button = fixture.nativeElement.querySelector('button');
+    expect(button.disabled).toBe(true);
+  });
 });
